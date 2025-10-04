4b9456b9b26ade976781fd2ab094cd5e2a1c0942
TruongSoCute <qtruong.hs@gmail.com>
2025-10-04 23:17:56 +0700

    change day 2

---
+++ Learning/learning_day2/lib/models/category_models.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class CategoryModel {
+  String name;
+  String iconPath;
+  Color boxColor;
+
+  CategoryModel({
+    required this.name,
+    required this.iconPath,
+    required this.boxColor,
+  });
+
+  List<CategoryModel> getCategory() {
+    List<CategoryModel> category = [];
+    return category;
+  }
+}
